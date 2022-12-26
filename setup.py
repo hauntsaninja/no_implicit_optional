f76d963cd7f52a6de55b442287517bb8966e9fb9
hauntsaninja <hauntsaninja@gmail.com>
2022-12-26 00:10:14 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="no_implicit_optional",
-    version="1.2",
+    version="1.3",
     author="Shantanu Jain",
     author_email="hauntsaninja@gmail.com",
     description="A codemod to make your implicit optional type hints PEP 484 compliant.",
